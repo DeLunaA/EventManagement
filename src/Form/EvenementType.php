53337a23610231021b2986a8270e52df7f118d5a
sirine <sirinebenjemaa12@gmail.com>
2024-03-01 11:41:24 +0100

    controle saisie+affichage client side

--- src/Form/EvenementType.php
+++ src/Form/EvenementType.php
@@ -6,16 +6,40 @@ use App\Entity\Evenement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+
 
 class EvenementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date')
-            ->add('titre')
-            ->add('description')
-            ->add('lieu')
+            ->add('date', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Date is required']), 
+                ],
+            ])
+            ->add('titre', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Titre is required']),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Title must be at least {{ limit }} characters long',
+                    ]),  
+                ],
+            ])
+            ->add('description', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Description is required']), 
+                ],
+            ])
+            ->add('lieu', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Lieu is required']), 
+                ],
+            ])
             ->add('participation')
         ;
     }
